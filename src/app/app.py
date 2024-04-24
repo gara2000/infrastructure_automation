67fc661632e7c9765e8ee7b9b534f7d7ba8745e3
me <me@me.me>
2024-04-25 00:40:18 +0200

    test github actions pipeline

--- src/app/app.py
+++ src/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Helloooo!'
+    return 'Hellooo!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
