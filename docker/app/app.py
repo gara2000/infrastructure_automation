2ae96f8e9481ca0ba156b4d5c6b8318cf4577797
gara <firasgara@hotmail.fr>
2024-05-01 01:59:16 +0200

    updated application

--- docker/app/app.py
+++ docker/app/app.py
@@ -1,10 +1,83 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello():
+#     return 'Hellooo!'
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8080, debug=True)
+
+from flask import Flask, request, jsonify
+
+import pandas as pd
+import joblib
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import GradientBoostingClassifier
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hellooo!'
+def scale(payload):
+    """Scales Payload"""
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.logger.info("Scaling Payload: %s", payload)
+    scaler = StandardScaler().fit(payload)
+    scaled_adhoc_predict = scaler.transform(payload)
+    return scaled_adhoc_predict
+
+
+@app.route("/")
+def home():
+    html = (
+        "<h3>Welcome to Sklearn Bostong Housing Price Prediction</h3>"
+    )
+    return html.format(format)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    """Performs a sklearn prediction
+
+    input looks like:
+            {
+    "CHAS":{
+      "0":0
+    },
+    "RM":{
+      "0":6.575
+    },
+    "TAX":{
+      "0":296.0
+    },
+    "PTRATIO":{
+       "0":15.3
+    },
+    "B":{
+       "0":396.9
+    },
+    "LSTAT":{
+       "0":4.98
+    }
+
+    result looks like:
+    { "prediction": [ 30.084999999999987 ] }
+
+    """
+
+    try:
+        clf = joblib.load("boston_housing_prediction.joblib")
+    except Exception as e:
+        app.logger.info("Failed to load model: %s", e)
+        return "Model not loaded"
+
+    json_payload = request.json
+    app.logger.info("JSON payload: %s", json_payload)
+    inference_payload = pd.DataFrame(json_payload)
+    app.logger.info("Inference payload DataFrame: %s",  inference_payload)
+    scaled_payload = scale(inference_payload)
+    prediction = list(clf.predict(scaled_payload))
+    return jsonify({"prediction": prediction})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
--- test_scripts/curl_webserver.sh
+++ test_scripts/curl_webserver.sh
@@ -3,6 +3,32 @@
 DIRNAME=$(dirname $0)
 cd $DIRNAME
 
-IP=$(./get_ip.sh)
+HOST=$(./get_ip.sh)
 PORT=8080
-curl "http://$IP:$PORT"
+echo "==== Curling the Home page ===="
+curl "http://$HOST:$PORT"
+
+echo "==== Making a prediction ===="
+# POST method predict
+curl -d '{  
+   "CHAS":{  
+      "0":0
+   },
+   "RM":{  
+      "0":6.575
+   },
+   "TAX":{  
+      "0":296.0
+   },
+   "PTRATIO":{  
+      "0":15.3
+   },
+   "B":{  
+      "0":396.9
+   },
+   "LSTAT":{  
+      "0":4.98
+   }
+}'\
+     -H "Content-Type: application/json" \
+     -X POST http://$HOST:$PORT/predict
